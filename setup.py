d9fdf1eac1c32c778e1a018d093cd4081f9d1ef2
LING71671 <1739677116@qq.com>
2026-02-21 01:03:02 +0800

    chore: Remove test files and exclude them from package build

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/LING71671/faker-cn",
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests*", "testing*"]),
     include_package_data=True,
     package_data={
         "faker_cn": ["*.json", "*.gz"],
