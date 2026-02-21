453c79cb74d6df96a3c5a8a98f6d2dcf774be035
LING71671 <1739677116@qq.com>
2026-02-22 03:28:07 +0800

    chore: release v0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="faker-cn",
-    version="0.1.2",
+    version="0.1.3",
     author="LING71671",
     author_email="ling71671@gmail.com",
     description="The most rigorous and realistic Chinese Persona Provider for Python Faker.",
